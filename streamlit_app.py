0a4f14c1e31996119c9a33ee05e1c986af1e7368
igivis7 <igor.isaev@gmx.net>
2022-07-14 18:43:52 +0200

    more debug information is added

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,17 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import models
 
+## For debug
+##--------------------------------
+import os 
+import psutil
+# import gc
+# import sys
+# gc.collect()
+# sys.modules[__name__].__dict__.clear()
+
+
+
 
 
 
@@ -353,3 +364,68 @@ print(f"Model Prediction Output Index: {pred_index}")
 
 st.write(uploaded_audio_file_4debug)
 print(uploaded_audio_file_4debug)
+
+
+## Show memory usage
+##--------------------------------
+process = psutil.Process(os.getpid())
+mem_used_by_app = process.memory_info().rss /1024 /1024  # in MBytes 
+print(f"The app uses: {mem_used_by_app} MB")
+st.markdown(f"The app uses: {mem_used_by_app} MB")
+
+st.write("To see machine info check the console output")
+
+
+
+
+
+
+
+
+# CPU information
+##--------------------------------
+print("="*40, "CPU Info", "="*40)
+# number of cores
+print("Physical cores:", psutil.cpu_count(logical=False))
+print("Total cores:", psutil.cpu_count(logical=True))
+## CPU frequencies
+# cpufreq = psutil.cpu_freq()
+# print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+# print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
+# print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+# CPU usage
+print("CPU Usage Per Core:")
+for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+    print(f"Core {i}: {percentage}%")
+print(f"Total CPU Usage: {psutil.cpu_percent()}%")
+
+
+## Memory Information
+##--------------------------------
+def get_size(bytes, suffix="B"):
+    """
+    Scale bytes to its proper format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+print("="*40, "Memory Information", "="*40)
+# get the memory details
+svmem = psutil.virtual_memory()
+print(f"Total: {get_size(svmem.total)}")
+print(f"Available: {get_size(svmem.available)}")
+print(f"Used: {get_size(svmem.used)}")
+print(f"Percentage: {svmem.percent}%")
+print("="*20, "SWAP", "="*20)
+# get the swap memory details (if exists)
+swap = psutil.swap_memory()
+print(f"Total: {get_size(swap.total)}")
+print(f"Free: {get_size(swap.free)}")
+print(f"Used: {get_size(swap.used)}")
+print(f"Percentage: {swap.percent}%")
